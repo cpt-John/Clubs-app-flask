f6de2dec7cdb5547bc896fceeda28ecbdcbf5e8a
John Francis <johnfrancis95815@gmail.com>
2020-08-09 18:48:17 +0530

    initial

---
+++ index.py
@@ -0,0 +1,114 @@
+from flask import Flask, request, Response, render_template
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost/clubs-app'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Clubs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey(
+        'categories.id'), nullable=False)
+    category = db.relationship('Categories', foreign_keys=[category_id])
+    public = db.Column(db.Boolean, nullable=False)
+
+
+class Categories(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+
+def add_club(clubName, category=1, public=0):
+    try:
+        club = Clubs(
+            name=clubName, category_id=category, public=public
+        )
+        db.session.add(club)
+        db.session.commit()
+        return 'added'
+    except:
+        return 'error in adding'
+
+
+def update_club(club_id, clubName, category, public):
+    try:
+        club = Clubs.query.get(club_id)
+        club.name = clubName
+        club.category_id = category
+        club.public = public
+        db.session.commit()
+        return 'updated'
+    except:
+        return 'error in updating'
+
+
+def delete_club(club_id):
+    try:
+        club = Clubs.query.get(club_id)
+        db.session.delete(club)
+        db.session.commit()
+        return 'deleted'
+    except:
+        return 'error in deleting'
+
+
+def get_categories():
+    array_categories = []
+    try:
+        categories = Categories.query.all()
+        for category in categories:
+            array_categories.append({'id': category.id, 'name': category.name})
+    except:
+        array_categories = []
+    finally:
+        return array_categories
+
+
+def get_clubs():
+    array_clubs = []
+    try:
+        clubs = Clubs.query\
+            .join(Categories, Clubs.category_id == Categories.id).all()
+
+        for club in clubs:
+            array_clubs.append({'id': club.id, 'name': club.name,
+                                'category': club.category.name, 'category_id': club.category.id, 'public': club.public, 'public_id': int(club.public)})
+    except:
+        array_clubs = []
+    finally:
+        return array_clubs
+
+
+@app.route("/", methods=['GET'])
+def clubs():
+    return render_template('dash.html', clubs=get_clubs(), categories=get_categories())
+
+
+@app.route("/addClub", methods=['POST'])
+def addclub():
+    if not request.json['clubName']:
+        return Response({'message': 'bad request'}, status=400, mimetype='application/json')
+    return add_club(request.json['clubName'], int(request.json['category']), int(request.json['public']))
+
+
+@app.route("/updateClub", methods=['POST'])
+def updateclub():
+    if not request.json['club_id']:
+        return Response({'message': 'bad request'}, status=400, mimetype='application/json')
+    return update_club(int(request.json['club_id']), request.json['clubName'], int(request.json['category']), int(request.json['public']))
+
+
+@app.route("/deleteClub", methods=['DELETE'])
+def deleteclub():
+    club_id = request.headers.get('club_id')
+    if not club_id:
+        return Response({'message': 'bad request'}, status=400, mimetype='application/json')
+    return delete_club(int(club_id))
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/dash/dash.js
@@ -0,0 +1,94 @@
+$(function () {
+  $(".nav-link").click(function () {
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+    if ($(this).attr("val") == "form") {
+      $("#form").show();
+      $("#clubs").hide();
+    }
+    if ($(this).attr("val") == "clubs") {
+      $("#form").hide();
+      $("#clubs").show();
+    }
+  });
+  $("#search").on("input", function () {
+    let value = $(this).val();
+    if (!value) {
+      $("tbody>tr").show();
+      return;
+    }
+    let regex = new RegExp("^" + value, "i");
+    $("tbody>tr").each(function () {
+      if (!$(this).attr("class").match(regex)) {
+        $(this).hide();
+      }
+    });
+  });
+  $(".edit-btn").click(function () {
+    let id = $(this).attr("club_id");
+    let clubName = $(this).attr("clubName");
+    let category = $(this).attr("category");
+    let public = $(this).attr("public");
+    $("#m_clubName").val(clubName);
+    $("#m_categories").val(category).change();
+    $("#m_public").val(public).change();
+    $(".update-btn").attr("club_id", id);
+    $(".delete-btn").attr("club_id", id);
+  });
+  $(".update-btn").click(function () {
+    let id = $(this).attr("club_id");
+    updateClub(
+      id,
+      $("#m_clubName").val(),
+      $("#m_categories").val(),
+      $("#m_public").val()
+    );
+    $("#updateModal").modal("hide");
+  });
+  $(".delete-btn").click(function () {
+    let id = $(this).attr("club_id");
+    deleteClub(id);
+    $("#updateModal").modal("hide");
+  });
+});
+
+function addClub() {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      alert(this.response);
+    }
+  };
+  xhr.open("POST", "http://127.0.0.1:5000/addClub", true);
+  let clubName = $("#clubName").val();
+  let category = $("#categories").val();
+  let public = $("#public").val();
+  let body = { clubName, category, public };
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.send(JSON.stringify(body));
+}
+
+function updateClub(club_id, clubName, category, public) {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      alert(this.response);
+    }
+  };
+  xhr.open("POST", "http://127.0.0.1:5000/updateClub", true);
+  let body = { club_id, clubName, category, public };
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.send(JSON.stringify(body));
+}
+
+function deleteClub(club_id) {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      alert(this.response);
+    }
+  };
+  xhr.open("DELETE", "http://127.0.0.1:5000/deleteClub", true);
+  xhr.setRequestHeader("club_id", `${club_id}`);
+  xhr.send();
+}
